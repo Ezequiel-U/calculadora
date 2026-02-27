e5b1f216037389eef5c21a2fff1b045999da9c90
Ezequiel-U <urietaezequiel16@gmail.com>
2026-02-27 10:41:02 -0700

    Agrega CI pipeline

---
+++ test/test_calculador.py
@@ -0,0 +1,18 @@
+from calculator import suma, resta, multiplicacion, division
+import pytest
+
+def test_suma():
+    assert suma(2, 3) == 5
+
+def test_resta():
+    assert resta(5, 2) == 3
+
+def test_multiplicacion():
+    assert multiplicacion(4, 3) == 12
+
+def test_division():
+    assert division(10, 2) == 5
+
+def test_division_por_cero():
+    with pytest.raises(ValueError):
+        division(5, 0)
